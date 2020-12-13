c7f9f56d0acfa70b2124626b4e45cd730c61da13
Jason <38218303+JasonRBeer@users.noreply.github.com>
2020-12-13 12:19:23 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file ('/opt/minecraft':
     ensure => directory,
     )
-  file ('opt/minecraft/minecraft_server.jar':
+  file ('/opt/minecraft/minecraft_server.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',  
   )
@@ -16,5 +16,9 @@ class minecraft {
   file ('/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/mincraft/minecraft.service',
+  )
+  service ('minecraft':
+    ensure => running,
+    enable => true,
   )
 }
